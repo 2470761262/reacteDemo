c070b8770dd1fb2600ac43bf9269316240f1a8d2
247076126@qq.com <247076126@qq.com>
2020-06-04 21:56:18 +0800

    学习react的生命周期

--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -11,6 +11,7 @@ class TodoItem extends Component {
         this.removeItem = this.removeItem.bind(this);
     }
     render () {
+        console.log("child ==> render");
          return <li  onClick={this.removeItem}>
              {this.props.text}/{this.props.index}
             </li>
@@ -19,14 +20,55 @@ class TodoItem extends Component {
     removeItem(){
         this.props.parentRemoveIten(this.props.index)
     }
+
+
+    // 这个生命周期执行的条件是
+    // 1.当前这个组件接收了父组件传递的参数
+    // 2.父组件的render函数重新执行了,这个生命周期才会执行
+    // 3.一定要之前存在。才会执行
+    UNSAFE_componentWillReceiveProps(){
+        console.log("child ==> componentWillReceiveProps");
+    }    
+
+    // 在组件即将被挂载到页面的时刻执行  只在第一次挂载的时候执行
+    UNSAFE_componentWillMount(){
+        console.log("child ==> componentWillMount ==> 在组件即将被挂载到页面的时刻执行");
+    }
+    // 在组件挂载完成之后执行 只在第一次挂载的时候执行
+    componentDidMount(){
+        console.log("child ==> componentDidMount ==> 在组件挂载完成之后执行");
+    }
+
+    // 只有在组件被更新之前执行 需要返回一个bool true 更新 false 不更新
+    // false 之后的生命周期函数将都不会执行
+    // componentWillUpdate render componentDidUpdate 将不执行
+    shouldComponentUpdate(){
+        console.log("child ==> shouldComponentUpdate ==> 组件被更新之前执行");
+        return true;
+    }
+
+    //在组件更新之前 shouldComponentUpdate执行之后 执行
+    UNSAFE_componentWillUpdate(){
+        console.log("child ==> componentWillUpdate ==> 在组件更新之前并且shouldComponentUpdate之后执行");
+    }
+
+    //组件更新完成之后执行 也就是render执行完成之后
+    componentDidUpdate(){
+        console.log("child ==> componentDidUpdate ==> 组件更新完成之后执行");
+    }
+
+     //在组件被移除的前一刻执行
+     componentWillUnmount(){
+        console.log("child ==> componentWillUnmount ==> 在组件被移除的前一刻执行");
+    }
 }
 
 //设置Props 传递属性类型
-//PropTypes.arrayOf 可以联合类型
+//PropTypes.oneOfType 可以联合类型
 // react 有点傻逼
 TodoItem.propTypes = {
     text: PropTypes.string.isRequired,
-    index:PropTypes.arrayOf(PropTypes.number,PropTypes.string) ,
+    index:PropTypes.oneOfType([PropTypes.number,PropTypes.string]) ,
     parentRemoveIten:PropTypes.func
 }
 
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -6,7 +6,7 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
 
-        //给inputChange重新设置this指向
+        // 给inputChange重新设置this指向
 
         this.inputChange = this.inputChange.bind(this);
 
@@ -18,14 +18,76 @@ class TodoList extends Component {
         //state就是vue 的data里的数据
         this.state = {
             inputValue: "",
-            list: []
+            list: ['谢谢你']
         }
     }
+    // 这个生命周期执行的条件是
+    // 1.当前这个组件接收了父组件传递的参数
+    // 2.父组件的render函数重新执行了,这个生命周期才会执行
+    // 3.一定要之前存在。才会执行
+    UNSAFE_componentWillReceiveProps(){
+        console.log("componentWillReceiveProps");
+    }    
 
-    //input输入变化的监听事件
+    // 在组件即将被挂载到页面的时刻执行  只在第一次挂载的时候执行
+    UNSAFE_componentWillMount(){
+        console.log("componentWillMount ==> 在组件即将被挂载到页面的时刻执行");
+    }
+    // 在组件挂载完成之后执行 只在第一次挂载的时候执行
+    componentDidMount(){
+        console.log("componentDidMount ==> 在组件挂载完成之后执行");
+    }
+
+    // 只有在组件被更新之前执行 需要返回一个bool true 更新 false 不更新
+    // false 之后的生命周期函数将都不会执行
+    // componentWillUpdate render componentDidUpdate 将不执行
+    shouldComponentUpdate(){
+        console.log("shouldComponentUpdate ==> 组件被更新之前执行");
+        return true;
+    }
+
+    //在组件更新之前 shouldComponentUpdate执行之后 执行
+    UNSAFE_componentWillUpdate(){
+        console.log("componentWillUpdate ==> 在组件更新之前并且shouldComponentUpdate之后执行");
+    }
+
+    render (h) {
+        console.log("render");
+        return <section>
+            <input
+                type="text"
+                placeholder="来给劳资输入点阳间的东西"
+                value={this.state.inputValue}
+                onChange={this.inputChange}
+                ref={(e)=>{ this.inputDom = e}}
+            />
+            <button onClick={this.inputCheckSubmit}>提交</button>
+            <ul>
+                {
+                    this.getTodiItem()
+                }
+            </ul>
+        </section>
+
+    }
+
+    //组件更新完成之后执行 也就是render执行完成之后
+    componentDidUpdate(){
+        console.log("componentDidUpdate ==> 组件更新完成之后执行");
+    }
+
+    //在组件被移除的前一刻执行
+    componentWillUnmount(){
+        console.log("componentWillUnmount ==> 在组件被移除的前一刻执行");
+    }
+
+
+    // input输入变化的监听事件
     inputChange (e) {
+
         //使用setState修改state里的数据
         const value = e.target.value;
+
         this.setState(() => {
             return {
                 inputValue: value
@@ -33,19 +95,31 @@ class TodoList extends Component {
         })
     }
 
-    //确定提交当前的input内容
+    // 确定提交当前的input内容
     inputCheckSubmit () {
+
+         //使用ref来获取dom
+
+         console.log(this.inputDom,"this.inputDom");
+
+         //不能像Vue可以把ref挂载组件上获取组件实例，react 获得的是undefined
+ 
+         console.log(this.TodoItemDom,"this.TodoItemDom");
+
+
         //prevState代表修改数据之前的数据是怎么样的
         this.setState((prevState) => {
             return {
                 list: [...prevState.list, prevState.inputValue],
                 inputValue: ''
             }
+        },()=>{
+            console.log('这里是setState的回调,代表页面已经更新完毕');
         })
 
     }
 
-    //删除当前的某一行
+    // 删除当前的某一行
     removeItem (index) {
        
         //prevState代表修改数据之前的数据是怎么样的
@@ -86,27 +160,11 @@ class TodoList extends Component {
                     index={index}
                     key={index}
                     parentRemoveIten={this.removeItem}
+                    ref={(e)=> this.TodoItemDom = e}
                 ></TodoItem>)
         })
     }
-
-    render (h) {
-        return <section>
-            <input
-                type="text"
-                placeholder="来给劳资输入点阳间的东西"
-                value={this.state.inputValue}
-                onChange={this.inputChange}
-            />
-            <button onClick={this.inputCheckSubmit}>提交</button>
-            <ul>
-                {
-                    this.getTodiItem()
-                }
-            </ul>
-        </section>
-
-    }
+    
 }
 
 export default TodoList;
